629a9b118f71fadb0e683ce0410fc04654c55067
gauravmehra-005 <gaurav.mehra@wissen.com>
2025-04-23 15:40:13 +0530

    cleaned up HRController by moving business logic to service layer

--- BackEnd/hr-service/src/main/java/com/example/demo/controller/HRController.java
+++ BackEnd/hr-service/src/main/java/com/example/demo/controller/HRController.java
@@ -2,20 +2,11 @@ package com.example.demo.controller;
 
 
 import java.util.List;
-import java.util.Base64;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
+
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,14 +15,11 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.HttpStatusCodeException;
-import org.springframework.web.client.RestTemplate;
+
 
 import com.example.demo.entity.Employee;
 import com.example.demo.entity.HR;
 import com.example.demo.service.HRService;
-
-import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/hr")
@@ -39,11 +27,6 @@ public class HRController {
 
     @Autowired
     private HRService hrService;
-    
-    @Autowired
-    private RestTemplate restTemplate;
-    
-    private static final Logger logger = LoggerFactory.getLogger(HRController.class);
     
     @PostMapping("/register")
     public HR registerHR(@RequestBody HR hr) {
@@ -98,102 +81,23 @@ public class HRController {
     //Employee CRUD Operations
     
 	@PostMapping("/register-employee")
-	public ResponseEntity<String> registerEmployeeFromHR(@RequestBody Employee employee, HttpServletRequest request) {
-		String employeeServiceUrl = "http://employee-service/employee";
-	    Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-	    HR hr = hrService.getHRByEmail(auth.getName());
-	    employee.setHrId(hr.getHrId());
-	    employeeServiceUrl+="/register-employee";
-	   
-	    HttpHeaders headers = new HttpHeaders();
-	    headers.setContentType(MediaType.APPLICATION_JSON);
-	    String sessionId = request.getSession().getId();
-	    
-	    // Base64 encode the session ID
-	    String encodedSessionId = Base64.getEncoder().encodeToString(sessionId.getBytes());
-	    logger.info("Base64 Encoded Session ID: {}", encodedSessionId);
-	    headers.add("Cookie", "SESSION=" + encodedSessionId + "; Path=/; HttpOnly");
-	    HttpEntity<Employee> httpEntity = new HttpEntity<>(employee, headers);
-	
-	    try {
-	        ResponseEntity<String> response = restTemplate.exchange(
-	            employeeServiceUrl,
-	            HttpMethod.POST,
-	            httpEntity,
-	            String.class
-	        );
-	        return ResponseEntity.status(response.getStatusCode()).body(response.getBody());
-	    } catch (HttpStatusCodeException ex) {
-	        return ResponseEntity.status(ex.getStatusCode()).body(ex.getResponseBodyAsString());
-	    } catch (Exception e) {
-	        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-	                .body("Error communicating with Employee Service: " + e.getMessage());
-	    }
+	public ResponseEntity<String> registerEmployeeFromHR(@RequestBody Employee employee) {
+		return hrService.registerEmployeeFromHR(employee);
 	}
     
     @PutMapping("/update-employee")
-    public ResponseEntity<String> updateEmployee(@RequestBody Employee employee,HttpServletRequest request) {
-    	String employeeServiceUrl = "http://employee-service/employee";
-    	Authentication auth=SecurityContextHolder.getContext().getAuthentication();
-        HR hr = hrService.getHRByEmail(auth.getName());
-        if (hr.getHrId()!=employee.getHrId()) {
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("You do not have authorization to update this employee");
-        }
-        try {
-        	employee.setHrId(hr.getHrId());
-			employeeServiceUrl+="/update-employee";
-			HttpHeaders headers = new HttpHeaders();
-			headers.setContentType(MediaType.APPLICATION_JSON);
-			String sessionId = request.getSession().getId();
-			    // Base64 encode the session ID
-			String encodedSessionId = Base64.getEncoder().encodeToString(sessionId.getBytes());
-			headers.add("Cookie", "SESSION=" + encodedSessionId + "; Path=/; HttpOnly");
-			HttpEntity<Employee> httpEntity = new HttpEntity<>(employee, headers);
-			ResponseEntity<String> response=restTemplate.exchange(employeeServiceUrl,HttpMethod.PUT, httpEntity,String.class);
-            return ResponseEntity.status(response.getStatusCode()).body(response.getBody());
-        } catch (RuntimeException ex) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
-        }
+    public ResponseEntity<String> updateEmployee(@RequestBody Employee employee) {
+    	return hrService.updateEmployee(employee);
     }
 
     @GetMapping("/all-employee")
-    public ResponseEntity<List<Employee>> getAllEmployees(HttpServletRequest request) {
-       String employeeServiceUrl = "http://employee-service/employee";
-    	Authentication auth=SecurityContextHolder.getContext().getAuthentication();
-        HR hr = hrService.getHRByEmail(auth.getName());
-        employeeServiceUrl+="/all-employee/"+hr.getHrId();
-        HttpHeaders headers=new HttpHeaders();
-        String sessionId=request.getSession().getId();
-        String encodedSessionId=Base64.getEncoder().encodeToString(sessionId.getBytes());
-        headers.add("Cookie", "SESSION="+encodedSessionId+"; Path=/; HttpOnly");
-        HttpEntity<Employee> entity=new HttpEntity<>(headers);
-        ResponseEntity<List<Employee>> response = restTemplate.exchange(
-        	    employeeServiceUrl,
-        	    HttpMethod.GET,
-        	    entity,
-        	    new ParameterizedTypeReference<List<Employee>>() {}
-        	);
-
-        return ResponseEntity.status(response.getStatusCode()).body(response.getBody());
+    public ResponseEntity<List<Employee>> getAllEmployees() {
+       return hrService.getAllEmployees();
     }
     
     
     @DeleteMapping("/delete-employee/{eid}")
-    public ResponseEntity<String> deleteEmployee(@PathVariable Long eid,HttpServletRequest request){
-    	 	String employeeServiceUrl = "http://employee-service/employee";
-    	 employeeServiceUrl+="/delete-employee/"+eid;
-         HttpHeaders headers=new HttpHeaders();
-         String sessionId=request.getSession().getId();
-         String encodedSessionId=Base64.getEncoder().encodeToString(sessionId.getBytes());
-         headers.add("Cookie", "SESSION="+encodedSessionId+"; Path=/; HttpOnly");
-         HttpEntity<Employee> entity=new HttpEntity<>(headers);
-         ResponseEntity<String> response = restTemplate.exchange(
-         	    employeeServiceUrl,
-         	    HttpMethod.DELETE,
-         	    entity,
-         	    String.class
-         	);
-
-         return ResponseEntity.status(response.getStatusCode()).body(response.getBody());
+    public ResponseEntity<String> deleteEmployee(@PathVariable Long eid){
+    	 return hrService.deleteEmployee(eid);
     }
 }
--- BackEnd/hr-service/src/main/java/com/example/demo/service/HRService.java
+++ BackEnd/hr-service/src/main/java/com/example/demo/service/HRService.java
@@ -1,13 +1,32 @@
 package com.example.demo.service;
 
+import java.util.Base64;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import com.example.demo.entity.Employee;
 import com.example.demo.entity.HR;
 import com.example.demo.repo.HRRepository;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Service
 public class HRService {
@@ -16,6 +35,9 @@ public class HRService {
     private HRRepository hrRepository;
     @Autowired
     private PasswordEncoder passwordEncoder;
+    
+    @Autowired
+    RestTemplate restTemplate;
   
     public HR registerHR(HR hr) {
     	hr.setEmail(hr.getEmail().toLowerCase());
@@ -48,4 +70,107 @@ public class HRService {
     public HR getHrById(Long id) {
         return hrRepository.findById(id).orElse(null);
     }
+    
+    //Employee CRUD
+	public ResponseEntity<String> registerEmployeeFromHR( Employee employee) {
+		String employeeServiceUrl = "http://employee-service/employee";
+	    Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+	    HR hr = getHRByEmail(auth.getName());
+	    employee.setHrId(hr.getHrId());
+	    employeeServiceUrl+="/register-employee";
+	   
+	    HttpHeaders headers = new HttpHeaders();
+	    headers.setContentType(MediaType.APPLICATION_JSON);
+	    RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
+	    HttpServletRequest request = ((ServletRequestAttributes) requestAttributes).getRequest();
+	    String sessionId = request.getSession().getId();
+	    
+	    // Base64 encode the session ID
+	    String encodedSessionId = Base64.getEncoder().encodeToString(sessionId.getBytes());
+	    headers.add("Cookie", "SESSION=" + encodedSessionId + "; Path=/; HttpOnly");
+	    HttpEntity<Employee> httpEntity = new HttpEntity<>(employee, headers);
+	
+	    try {
+	        ResponseEntity<String> response = restTemplate.exchange(
+	            employeeServiceUrl,
+	            HttpMethod.POST,
+	            httpEntity,
+	            String.class
+	        );
+	        return ResponseEntity.status(response.getStatusCode()).body(response.getBody());
+	    } catch (HttpStatusCodeException ex) {
+	        return ResponseEntity.status(ex.getStatusCode()).body(ex.getResponseBodyAsString());
+	    } catch (Exception e) {
+	        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+	                .body("Error communicating with Employee Service: " + e.getMessage());
+	    }
+	}
+    
+    public ResponseEntity<List<Employee>> getAllEmployees() {
+        String employeeServiceUrl = "http://employee-service/employee";
+     	Authentication auth=SecurityContextHolder.getContext().getAuthentication();
+         HR hr = getHRByEmail(auth.getName());
+         employeeServiceUrl+="/all-employee/"+hr.getHrId();
+         HttpHeaders headers=new HttpHeaders();
+         RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
+         HttpServletRequest request = ((ServletRequestAttributes) requestAttributes).getRequest();
+         String sessionId = request.getSession().getId();
+         String encodedSessionId=Base64.getEncoder().encodeToString(sessionId.getBytes());
+         headers.add("Cookie", "SESSION="+encodedSessionId+"; Path=/; HttpOnly");
+         HttpEntity<Employee> entity=new HttpEntity<>(headers);
+         ResponseEntity<List<Employee>> response = restTemplate.exchange(
+         	    employeeServiceUrl,
+         	    HttpMethod.GET,
+         	    entity,
+         	    new ParameterizedTypeReference<List<Employee>>() {}
+         	);
+
+         return ResponseEntity.status(response.getStatusCode()).body(response.getBody());
+     }
+    
+    public ResponseEntity<String> updateEmployee(Employee employee) {
+    	String employeeServiceUrl = "http://employee-service/employee";
+    	Authentication auth=SecurityContextHolder.getContext().getAuthentication();
+        HR hr = getHRByEmail(auth.getName());
+        if (hr.getHrId()!=employee.getHrId()) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("You do not have authorization to update this employee");
+        }
+        try {
+        	employee.setHrId(hr.getHrId());
+			employeeServiceUrl+="/update-employee";
+			HttpHeaders headers = new HttpHeaders();
+			headers.setContentType(MediaType.APPLICATION_JSON);
+			 RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
+			 HttpServletRequest request = ((ServletRequestAttributes) requestAttributes).getRequest();
+			 String sessionId = request.getSession().getId();
+			    // Base64 encode the session ID
+			String encodedSessionId = Base64.getEncoder().encodeToString(sessionId.getBytes());
+			headers.add("Cookie", "SESSION=" + encodedSessionId + "; Path=/; HttpOnly");
+			HttpEntity<Employee> httpEntity = new HttpEntity<>(employee, headers);
+			ResponseEntity<String> response=restTemplate.exchange(employeeServiceUrl,HttpMethod.PUT, httpEntity,String.class);
+            return ResponseEntity.status(response.getStatusCode()).body(response.getBody());
+        } catch (RuntimeException ex) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+        }
+    }
+    
+    public ResponseEntity<String> deleteEmployee(Long eid){
+	 String employeeServiceUrl = "http://employee-service/employee";
+	 employeeServiceUrl+="/delete-employee/"+eid;
+     HttpHeaders headers=new HttpHeaders();
+     RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
+     HttpServletRequest request = ((ServletRequestAttributes) requestAttributes).getRequest();
+     String sessionId = request.getSession().getId();
+     String encodedSessionId=Base64.getEncoder().encodeToString(sessionId.getBytes());
+     headers.add("Cookie", "SESSION="+encodedSessionId+"; Path=/; HttpOnly");
+     HttpEntity<Employee> entity=new HttpEntity<>(headers);
+     ResponseEntity<String> response = restTemplate.exchange(
+     	    employeeServiceUrl,
+     	    HttpMethod.DELETE,
+     	    entity,
+     	    String.class
+     	);
+
+     return ResponseEntity.status(response.getStatusCode()).body(response.getBody());
+}
 }
